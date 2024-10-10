f42ee1034426a1f457623020460be3a460bd3dad
Dominic <Dominic@DESKTOP-0Q4EVDS>
2024-10-10 09:32:55 +0200

    kleine Änderungne zu control loop stability

--- control_loop_stability.m
+++ control_loop_stability.m
@@ -3,6 +3,8 @@ function [control_loop_stability] = control_loop_stability(G,K,S)
 % G(s) & K(s) needs to be proper and stable
 % S(s) needs to be stable 
 
+
+
 if(proper_tf(G)== true && proper_tf(K) == true && routh_hurwitz_tf(G) == true && routh_hurwitz_tf(K) == true && routh_hurwitz_tf(S) == true )
     control_loop_stability = true;
     disp('Control loop is stable');
