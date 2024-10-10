acdcba5df1ee9ef7ec5162cdf1ead2e62124cc6b
Dominic <Dominic@DESKTOP-0Q4EVDS>
2024-10-10 09:17:31 +0200

    Änderung an Code

--- proper_tf.m
+++ proper_tf.m
@@ -1,19 +1,11 @@
-function [proper, sternlyproper] = proper_tf(G)
-% Function to check if a transfer function is strictly proper or proper
+function is_proper = proper_tf(G)
+% Function to check if a transfer function is proper
 
 syms s; % set 's' as symbolic variable
 
 % Initialize output variables
-proper = false;
-sternlyproper = false;
+is_proper = false;
 
-    if limit(G, s, inf) == 0
-        % Transfer function is strictly proper
-        sternlyproper = true;
-        proper = true;
-    elseif limit(G, s, inf) < inf
-        % Transfer function is proper
-        sternlyproper = false;
-        proper = true;
-    end
+if limit(G, s, inf) < inf
+  is_proper = true;
 end
